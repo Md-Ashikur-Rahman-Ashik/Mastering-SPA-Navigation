565e1c44f8df333a425d0124eccd63b796a10e70
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-23 16:03:58 +0600

    React router dom installer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import "./App.css";
+import Blogs from "./pages/Blogs";
+import Bookmarks from "./pages/Bookmarks";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <Home />
+      <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
     </>
   );
 }
