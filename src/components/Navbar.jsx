140b273942cacf28cdfea517f812bc1039cf1b41
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-24 16:24:33 +0600

    Adding Hero Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [theme, setTheme] = useState("light");
@@ -6,9 +7,8 @@ const Navbar = () => {
   const handleToggle = (event) => {
     if (event.target.checked) {
       setTheme("synthwave");
-    }
-    else{
-        setTheme("light");
+    } else {
+      setTheme("light");
     }
   };
 
@@ -18,28 +18,23 @@ const Navbar = () => {
     const localTheme = localStorage.getItem("theme");
 
     document.querySelector("html").setAttribute("data-theme", localTheme);
-  }, [theme])
-
-  
+  }, [theme]);
 
   return (
     <div className="navbar bg-base-100 shadow-lg px-4 fixed z-10">
       <div className="flex-1">
-        <a className="btn btn-ghost gap-0 text-secondary normal-case text-2xl">
+        <Link
+          to="/"
+          className="btn btn-ghost gap-0 text-secondary normal-case text-2xl"
+        >
           Byte<span className="text-primary">Blaze</span>
-        </a>
+        </Link>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1 font-bold">
-          <li>
-            <a>Home</a>
-          </li>
-          <li className="text-primary">
-            <a>Blogs</a>
-          </li>
-          <li>
-            <a>Bookmarks</a>
-          </li>
+      <div className="flex-none gap-2">
+        <ul className="menu menu-horizontal px-1 font-bold hidden sm:flex gap-5">
+          <NavLink to="/" className={({isActive}) => isActive? "text-primary": "font-bold"}>Home</NavLink>
+          <NavLink to="/blogs" className={({isActive}) => isActive? "text-primary": "font-bold"}>Blogs</NavLink>
+          <NavLink to="/bookmarks" className={({isActive}) => isActive? "text-primary": "font-bold"}>Bookmarks</NavLink>
         </ul>
         <label className="cursor-pointer grid place-items-center">
           <input
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Blogs from "./pages/Blogs.jsx";
+import Bookmarks from "./pages/Bookmarks.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>
+      },
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>,
+      },
+      {
+        path: "/bookmarks",
+        element: <Bookmarks></Bookmarks>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
