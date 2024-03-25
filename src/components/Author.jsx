1e20d936bf236e51014384e90a3fbfd83860352c
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-25 16:15:38 +0600

    Conceptual session part five running

---
+++ src/components/Author.jsx
@@ -0,0 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+
+const Author = () => {
+
+    const blog = useLoaderData();
+
+    return (
+        <div>
+            <h1>{blog.user.name}</h1>            
+        </div>
+    );
+};
+
+export default Author;
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Blogs from "./pages/Blogs.jsx";
-import Bookmarks from "./pages/Bookmarks.jsx";
-import MainLayout from "./layouts/MainLayout.jsx";
-import Blog from "./pages/Blog.jsx";
+import { router } from "./Routes/Routes";
+import { RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home></Home>,
-      },
-      {
-        path: "/blogs",
-        element: <Blogs></Blogs>,
-        loader: () => fetch("https://dev.to/api/articles?per_page=20&top=7"),
-      },
-      {
-        path: "/blog/:id",
-        element: <Blog></Blog>,
-        loader: ({ params }) =>
-          fetch(`https://dev.to/api/articles/${params.id}`),
-      },
-      {
-        path: "/bookmarks",
-        element: <Bookmarks></Bookmarks>,
-      },
-    ],
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
