e4feb621f6738971e8e3b16e756dc60a188f17af
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-25 12:55:00 +0600

    Learning from fourth part of conceptual session

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home.jsx";
 import Blogs from "./pages/Blogs.jsx";
 import Bookmarks from "./pages/Bookmarks.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
+import Blog from "./pages/Blog.jsx";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,12 @@ const router = createBrowserRouter([
         path: "/blogs",
         element: <Blogs></Blogs>,
         loader: () => fetch("https://dev.to/api/articles?per_page=20&top=7"),
+      },
+      {
+        path: "/blog/:id",
+        element: <Blog></Blog>,
+        loader: ({ params }) =>
+          fetch(`https://dev.to/api/articles/${params.id}`),
       },
       {
         path: "/bookmarks",
